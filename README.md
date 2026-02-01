036635b9a9d65cf92d931f8b920c0551ef1075ca
Niels Nijholt <niels@nielsnijholt.nl>
2026-02-01 10:46:07 +0100

    Set license to MIT

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # effect-lib
 A Racket package that enables the usage of a sort of algebraic effects using a Freer monad.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- info.rkt
+++ info.rkt
@@ -1,15 +1,7 @@
 #lang info
 
-;; 1. Define the collection name explicitly
 (define collection "effect-lib")
-
-;; 2. Register documentation
-;; The path is relative to this info.rkt file
 (define scribblings '(("scribblings/manual.scrbl" (multi-page))))
-
-;; 3. Dependencies
 (define deps '("base"))
-
-;; 4. Build Dependencies
-;; Tools needed only for docs/tests/etc.
 (define build-deps '("scribble-lib" "racket-doc"))
+(define license "MIT")
