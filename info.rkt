b0a4047b60f5ef352f95be077fdb73062a8f6438
Niels Nijholt <niels@nielsnijholt.nl>
2026-02-08 15:32:33 +0100

    Added missing rackunit build dependency to info.rkt

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "freer-lib")
 (define scribblings '(("scribblings/manual.scrbl" (multi-page))))
 (define deps '("base" "gregor"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "A collection of libraries for writing freer monads in Racket.")
 (define pkg-authors '("Niels Nijholt"))
 (define license "MIT")
